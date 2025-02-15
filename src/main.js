229c7f180debcbcfa02fe51a81fa2a66f14bdeb0
goenning <me@goenning.net>
2025-02-15 20:25:43 +0000

    bootstrap

--- src/main.js
+++ src/main.js
@@ -3,16 +3,14 @@ const { invoke } = window.__TAURI__.core;
 let greetInputEl;
 let greetMsgEl;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+async function launchTerminal(name) {
+  await invoke("launch_terminal", { name});
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  const bt = document.getElementById("launch");
+  const terminal = document.getElementById("terminal");
+  bt.addEventListener("click", (e) => {
+    launchTerminal(terminal.value);
   });
 });
