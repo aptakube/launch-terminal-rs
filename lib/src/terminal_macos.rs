8cbc5bc66c676b6cd2c52b8bb53a0d15c451a1f3
Tobias Assarsson <tobias.assarsson@gmail.com>
2025-03-07 13:01:36 +0100

    simplify error handling in macos code

--- lib/src/terminal_macos.rs
+++ lib/src/terminal_macos.rs
@@ -1,7 +1,7 @@
+use std::collections::HashMap;
 use std::env::{self, home_dir};
 use std::path::Path;
 use std::process::{Command, Stdio};
-use std::collections::HashMap;
 use once_cell::sync::Lazy;
 
 use std::fs::{self, File};
@@ -10,9 +10,11 @@ use std::{env::temp_dir, io::Write, path::PathBuf};
 
 use crate::{Error, Terminal};
 
-const DEFAULT_SHELL: &str = "zsh";
-
-pub(crate) fn open(terminal: Terminal, command: &str, env_vars: HashMap<String, String>) -> Result<(), Error> {
+pub(crate) fn open(
+    terminal: Terminal,
+    command: &str,
+    env_vars: HashMap<String, String>,
+) -> Result<(), Error> {
     return match terminal {
         Terminal::AppleTerminal => open_with_app("terminal", command, env_vars),
         Terminal::ITerm2 => open_with_app("iterm", command, env_vars),
@@ -22,7 +24,7 @@ pub(crate) fn open(terminal: Terminal, command: &str, env_vars: HashMap<String, 
         Terminal::Tabby => open_with_tabby(command, env_vars),
         Terminal::WezTerm => open_with_wezterm(command, env_vars),
         _ => return Err(Error::NotSupported),
-    }
+    };
 }
 
 pub(crate) fn is_installed(terminal: Terminal) -> Result<bool, Error> {
@@ -37,29 +39,18 @@ pub(crate) fn is_installed(terminal: Terminal) -> Result<bool, Error> {
         _ => return Err(Error::NotSupported),
     };
 
-
-    let found = match Command::new("osascript")
-        .arg("-e")
-        .arg(format!("id of application \"{}\"", app_name))
+    Command::new("osascript")
+        .args(["-e", format!("id of application \"{}\"", app_name)])
         .stderr(Stdio::null())
         .stdin(Stdio::null())
         .stdout(Stdio::null())
         .status()
-    {
-        Ok(status) => status.success(),
-        Err(err) => return Err(Error::IOError(err)),
-    };
-
-   return Ok(found)
 }
 
 fn open_with_app(app: &str, command: &str, env_vars: HashMap<String, String>) -> Result<(), Error> {
     let path = write_temp_script(command, env_vars)?;
-
-    match Command::new("open").arg("-a").arg(app).arg(path).spawn() {
-        Ok(_) => Ok(()),
-        Err(err) => Err(Error::IOError(err)),
-    }
+    Command::new("open").args(["-a", app, path]).spawn()?;
+    Ok(())
 }
 
 fn open_with_wezterm(command: &str, env_vars: HashMap<String, String>) -> Result<(), Error> {
@@ -129,7 +120,7 @@ fn write_temp_script(command: &str, env_vars: HashMap<String, String>) -> Result
     let dir = temp_dir();
     let path = dir.join("run-in-terminal.sh");
 
-    let mut f = File::create(&path).map_err(Error::IOError)?;
+    let mut f = File::create(&path)?;
 
     let content = if command.is_empty() {
         format!("{}\n\ncd $HOME\n{} exec $SHELL", SHELL.to_string(), stringify_env_vars(env_vars))
@@ -137,18 +128,10 @@ fn write_temp_script(command: &str, env_vars: HashMap<String, String>) -> Result
         format!("{}\n\ncd $HOME\n{} {}\nexec $SHELL", SHELL.to_string(), stringify_env_vars(env_vars), command)
     };
 
-    f.write_all(content.as_bytes()).and_then(|_| f.flush()).map_err(Error::IOError)?;
+    f.write_all(content.as_bytes()).and_then(|_| f.flush())?;
 
     let permissions = fs::Permissions::from_mode(0o755);
-    fs::set_permissions(&path, permissions).map_err(Error::IOError)?;
+    fs::set_permissions(&path, permissions)?;
 
     Ok(path)
-}
-
-fn stringify_env_vars(env_vars: HashMap<String, String>) -> String {
-    env_vars
-        .iter()
-        .map(|(key, value)| format!("{}='{}'", key, value))
-        .collect::<Vec<String>>()
-        .join(" ")
 }
