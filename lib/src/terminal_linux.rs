3ff236c378340ed4483b1e6cd983fcfc704327a0
goenning <me@goenning.net>
2025-05-28 15:48:23 +0100

    start shell on $HOME

--- lib/src/terminal_linux.rs
+++ lib/src/terminal_linux.rs
@@ -154,9 +154,9 @@ fn write_temp_script(command: &str) -> Result<PathBuf, Error> {
     let mut f = File::create(&path).map_err(Error::IOError)?;
 
     let content = if command.is_empty() {
-        format!("#!/usr/bin/env sh\n\nexec $SHELL")
+        format!("#!/usr/bin/env sh\n\n cd $HOME\nexec $SHELL")
     } else {
-        format!("#!/usr/bin/env sh\n\n{}\nexec $SHELL", command)
+        format!("#!/usr/bin/env sh\n\n{}\n cd $HOME\nexec $SHELL", command)
     };
     f.write_all(content.as_bytes())
         .and_then(|_| f.flush())
